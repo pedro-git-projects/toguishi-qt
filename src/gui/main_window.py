1c37e04dcfd6eacda4f59797650cb669c10c72b4
pedro-git-projects <pedro.coding.contact@gmail.com>
2023-08-24 20:23:46 -0300

    cleaning up main function

--- src/gui/main_window.py
+++ src/gui/main_window.py
@@ -21,8 +21,6 @@ class MainWindow(QMainWindow):
 
         tab_widget = QTabWidget()
 
-        self.blade_form = BladeRegistrationForm()
-        self.dryer_form = DryerRegistrationForm()
         self.store_form = StoreForm(self.db_manager)
         self.customer_form = CustomerRegistrationForm(self.db_manager)
         self.service_item_form = ServiceItemForm(self.db_manager)
@@ -30,9 +28,6 @@ class MainWindow(QMainWindow):
         self.store_form.store_saved.connect(self.service_item_form.update_store_combo)
         self.store_form.store_saved.connect(self.customer_form.update_store_combo)
 
-
-        tab_widget.addTab(self.blade_form, "Lâminas")
-        tab_widget.addTab(self.dryer_form, "Secadores")
         tab_widget.addTab(self.store_form, "Loja")
         tab_widget.addTab(self.customer_form, "Cliente")
         tab_widget.addTab(self.service_item_form, "Item de Serviço")
