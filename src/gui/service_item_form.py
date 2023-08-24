658a358f2ca4b74780f9e692c7fc3212db00a843
pedro-git-projects <pedro.coding.contact@gmail.com>
2023-08-24 19:39:21 -0300

    updating service item form layout

--- src/gui/service_item_form.py
+++ src/gui/service_item_form.py
@@ -1,8 +1,9 @@
-from PySide6.QtCore import Qt
 from PySide6.QtWidgets import (
     QButtonGroup,
     QComboBox,
+    QHBoxLayout,
     QLabel,
+    QLineEdit,
     QRadioButton,
     QStackedWidget,
     QVBoxLayout,
@@ -21,68 +22,98 @@ from service.payment import Payment
 class ServiceItemForm(QWidget):
     def __init__(self, db_manager: DBManager):
         super().__init__()
-
         self.db_manager = db_manager
 
+        self.setup_ui()
+
+    def setup_ui(self):
         layout = QVBoxLayout()
-        layout.setSpacing(10)
-        layout.setAlignment(Qt.AlignmentFlag.AlignTop)
         self.setLayout(layout)
 
+        self.setup_store_section(layout)
+        self.setup_category_section(layout)
+        self.setup_price_section(layout)
+        self.setup_payment_section(layout)
+        self.setup_item_type_section(layout)
+        self.setup_stacked_widget(layout)
+
+    def setup_store_section(self, layout):
         self.store_label = QLabel("Loja:")
-        layout.addWidget(self.store_label)
-
         self.store_combo = QComboBox()
-        layout.addWidget(self.store_combo)
-
         self.store_combo.currentIndexChanged.connect(self.update_selected_store)
         self.populate_store_combo()
 
-        self.category_label = QLabel("Categoria:")
-        layout.addWidget(self.category_label)
+        layout.addWidget(self.store_label)
+        layout.addWidget(self.store_combo)
 
+    def setup_category_section(self, layout):
+        self.category_label = QLabel("Categoria:")
         self.category_combo = QComboBox()
+        self.category_combo.currentIndexChanged.connect(self.update_selected_category)
         for category in Category:
             self.category_combo.addItem(str(category))
+
+        layout.addWidget(self.category_label)
         layout.addWidget(self.category_combo)
 
-        self.category_combo.currentIndexChanged.connect(self.update_selected_category)
+    def setup_price_section(self, layout):
+        price_layout = QHBoxLayout()
 
+        self.inital_price_edit = QLineEdit()
+        self.inital_price_edit.setPlaceholderText("Preço")
+
+        self.discount_edit = QLineEdit()
+        self.discount_edit.setPlaceholderText("Desconto")
+
+        price_layout.addWidget(self.inital_price_edit)
+        price_layout.addWidget(self.discount_edit)
+
+        layout.addLayout(price_layout)
+
+    def setup_payment_section(self, layout):
         self.payment_label = QLabel("Forma de pagamento:")
-        layout.addWidget(self.payment_label)
-
         self.payment_combo = QComboBox()
         for payment in Payment:
             self.payment_combo.addItem(str(payment))
+
+        layout.addWidget(self.payment_label)
         layout.addWidget(self.payment_combo)
 
-        self.item_type_label = QLabel("Item: ")
-        layout.addWidget(self.item_type_label)
+    def setup_item_type_section(self, layout):
+        item_type_layout = QHBoxLayout()
 
-        self.item_type_radio_group = QButtonGroup()
+        self.item_type_label = QLabel("Item: ")
         self.dryer_radio = QRadioButton("Secador")
         self.blade_radio = QRadioButton("Lâmina")
-        self.blade_radio = QRadioButton("Tesoura")
+        self.scissors_radio = QRadioButton("Tesoura")
+
+        self.item_type_radio_group = QButtonGroup()
         self.item_type_radio_group.addButton(self.dryer_radio)
         self.item_type_radio_group.addButton(self.blade_radio)
+        self.item_type_radio_group.addButton(self.scissors_radio)
 
-        layout.addWidget(self.dryer_radio)
-        layout.addWidget(self.blade_radio)
+        item_type_layout.addWidget(self.item_type_label)
+        item_type_layout.addWidget(self.dryer_radio)
+        item_type_layout.addWidget(self.blade_radio)
+        item_type_layout.addWidget(self.scissors_radio)
 
+        self.item_type_radio_group.buttonClicked.connect(self.update_selected_item_type)
+
+        layout.addLayout(item_type_layout)
+
+    def setup_stacked_widget(self, layout):
         self.stacked_widget = QStackedWidget()
-        layout.addWidget(self.stacked_widget)
-
         self.dryer_form = DryerRegistrationForm()
         self.blade_form = BladeRegistrationForm()
-
         self.stacked_widget.addWidget(self.dryer_form)
         self.stacked_widget.addWidget(self.blade_form)
 
-        self.item_type_radio_group.buttonClicked.connect(self.update_selected_item_type)
+        layout.addWidget(self.stacked_widget)
 
     def update_selected_category(self):
         selected_index = self.category_combo.currentIndex()
         selected_category = Category(selected_index + 1)
+        # Do something with selected_category
 
     def populate_store_combo(self):
         stores = self.db_manager.get_all_stores()
@@ -106,11 +137,6 @@ class ServiceItemForm(QWidget):
 
     def get_selected_item_data(self):
         current_form = self.stacked_widget.currentWidget()
-        if isinstance(current_form, DryerRegistrationForm):
+        if isinstance(current_form, (DryerRegistrationForm, BladeRegistrationForm)):
             data = current_form.get_item_data()
             print(data)
-        elif isinstance(current_form, BladeRegistrationForm):
-            data = current_form.get_item_data()
-            print(data)
-        else:
-            pass
