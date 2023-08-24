a0e486ecab0f6c4fe9c4d79f6fecc009220ba66e
pedro-git-projects <pedro.coding.contact@gmail.com>
2023-08-24 14:35:09 -0300

    row factory|store and customer retrival operations|unit testing

--- src/db/db_manager.py
+++ src/db/db_manager.py
@@ -4,6 +4,7 @@ import sqlite3
 class DBManager:
     def __init__(self) -> None:
         self.connection = sqlite3.connect("toguishi.db")
+        self.connection.row_factory = sqlite3.Row
         self.cursor = self.connection.cursor()
         self.setup_database()
 
@@ -45,6 +46,26 @@ class DBManager:
         store_id = self.cursor.lastrowid
         self.connection.commit()
         return store_id
+
+    def get_all_customers(self):
+        self.cursor.execute("SELECT * FROM customers")
+        customers = self.cursor.fetchall()
+        return customers
+
+    def get_all_stores(self):
+        self.cursor.execute("SELECT * FROM stores")
+        stores = self.cursor.fetchall()
+        return stores
+
+    def get_store_by_name(self, store_name):
+        self.cursor.execute("SELECT * FROM stores WHERE name = ?", (store_name,))
+        store = self.cursor.fetchone()
+        return dict(store) if store else None
+
+    def get_customer_by_name(self, customer_name):
+        self.cursor.execute("SELECT * FROM customers WHERE name = ?", (customer_name,))
+        customer = self.cursor.fetchone()
+        return dict(customer) if customer else None
 
     def __del__(self):
         self.connection.close()
--- src/db/test_db.py
+++ src/db/test_db.py
@@ -26,3 +26,26 @@ def test_store_save(db_manager):
     )
     store_id = store.save()
     assert store_id is not None
+
+
+def test_get_store_by_name(db_manager):
+    store = Store(
+        "Focinho Feliz", "Asa Norte SQN 216 BL A", [Phone("(61)986792020")], db_manager
+    )
+    store.save()
+    retrieved_store = db_manager.get_store_by_name("Focinho Feliz")
+    assert retrieved_store is not None
+    assert retrieved_store["name"] == "Focinho Feliz"
+
+
+def test_get_customer_by_name(db_manager):
+    store = Store(
+        "Focinho Feliz", "Asa Norte SQN 216 BL A", [Phone("(61)986792020")], db_manager
+    )
+    store.save()
+    customer = Customer("João da Silva", [Phone("(61)93213459")], store, db_manager)
+    customer.save()
+
+    retrieved_customer = db_manager.get_customer_by_name("João da Silva")
+    assert retrieved_customer is not None
+    assert retrieved_customer["name"] == "João da Silva"
